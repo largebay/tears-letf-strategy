67ff8e625834bf81c20d565a0d0d5180c11780ee
largebay <203953998+largebay@users.noreply.github.com>
2025-03-22 19:05:18 -0500

    refactor: update fs and path imports to use node built-in modules

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import { FREDObservationData } from './src/types';
 import { checkTriggers } from './src/tears_letf_strategy';
 
